088d0fe7a51c51ff91196202a9300b5e13d4eac3
IgnacioArtosCarrio <ignacio.artos@gmail.com>
2024-02-01 13:30:53 +0100

    cambios menu y botones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.rollapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rollapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/rollapp/login/Login.java
+++ app/src/main/java/com/example/rollapp/login/Login.java
@@ -1,10 +1,14 @@
 package com.example.rollapp.login;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import com.example.rollapp.R;
+import com.example.rollapp.VentanaPrincipal;
+
 
 public class Login extends AppCompatActivity {
 
@@ -12,5 +16,24 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button entrar = findViewById(R.id.login);
+        TextView crearCuenta = findViewById(R.id.create_account);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, VentanaPrincipal.class);
+                startActivity(intent);
+            }
+        });
+
+        crearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, CrearCuenta.class);
+                startActivity(intent);
+            }
+        });
     }
 }
